0f7573438d2c0875a4f3fe0202786561f8882265
Iris <irishenceaway@gmail.com>
2021-08-29 16:18:28 +1200

    Final Implementation of mobile-friendly website, with form validation and basic security updates

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'full-communism'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'test_key'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
